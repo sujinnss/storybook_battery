786666813f684fb793a3d4b1f14fa1d5f108f200
sjpark1 <sjpark1@nkia.co.kr>
2020-05-29 17:27:43 +0900

    #2 test

---
+++ src/stories/Hello.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Hello from "../components/Hello";
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+export default {
+    title: 'components|basic/Hello', // 스토리북에서 보여질 그룹과 경로를 명시
+    component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
+    decorators: [withKnobs] // 애드온 적용
+};
+
+export const hello = () => {
+    // knobs 만들기
+    const big = boolean('bold',false);
+    const name = text('name', 'Storybook');
+    return (
+        <Hello
+            name={name}
+            big={big}
+            onHello={action('onHello')}
+            onBye={action('onBye')}
+        />
+    );
+};
+// knobs 적용 되는 부분 만든곳
+hello.story = {
+    name: 'example'
+};
+
+export const standard = () => <Hello name="Storybook" />;
+export const big = () => <Hello name="Storybook" big />;
